0f572186c8a0e678526139d0e89674d2c998c33f
Jon Pascoe <jon.pascoe@me.com>
2016-05-08 09:11:10 +0100

    Make rails version less pessimistic

--- lib/gimbal/version.rb
+++ lib/gimbal/version.rb
@@ -1,5 +1,5 @@
 module Gimbal
-  RAILS_VERSION = '~> 4.2.0'
+  RAILS_VERSION = '~> 4.2'
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
 end
