3e457dc649e8d1ae3743a2309be241c3979d93d5
Jon Pascoe <jon.pascoe@me.com>
2016-05-09 22:10:14 +0100

    regexp

--- spec/features/new_project_spec.rb
+++ spec/features/new_project_spec.rb
@@ -13,7 +13,9 @@ RSpec.describe 'Create a new project' do
 
     it 'leaves the devise gem disabled' do
       gemfile = IO.read("#{project_path}/Gemfile")
-      expect(gemfile).to match /^\# gem "devise"$/
+      expect(gemfile).to match(
+                           /^\# gem "devise"$/
+                         )
     end
   end
 
