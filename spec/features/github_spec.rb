e825d4222e7b7e3f9d93cc5ed12d31830d416b1e
Jon Pascoe <jon.pascoe@me.com>
2016-05-09 23:27:18 +0100

    enable github ... disable devise skipping

--- spec/features/github_spec.rb
+++ spec/features/github_spec.rb
@@ -1,16 +1,16 @@
-# require 'spec_helper'
-#
-# RSpec.describe "GitHub" do
-#   before do
-#     drop_dummy_database
-#     remove_project_directory
-#   end
-#
-#   it "creates a project with --github option" do
-#     repo_name = 'test'
-#     run_gimbal("--github=#{repo_name}")
-#     setup_app_dependencies
-#
-#     expect(FakeGithub).to have_created_repo(repo_name)
-#   end
-# end
+require 'spec_helper'
+
+RSpec.describe "GitHub" do
+  before do
+    drop_dummy_database
+    remove_project_directory
+  end
+
+  it "creates a project with --github option" do
+    repo_name = 'test'
+    run_gimbal("--github=#{repo_name}")
+    setup_app_dependencies
+
+    expect(FakeGithub).to have_created_repo(repo_name)
+  end
+end
