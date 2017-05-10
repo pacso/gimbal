01922c55cdca5525007790626bd72238d33739e0
Jon Pascoe <jon.pascoe@me.com>
2017-05-10 20:18:43 +0100

    Bump gem version

--- lib/gimbal/version.rb
+++ lib/gimbal/version.rb
@@ -1,5 +1,5 @@
 module Gimbal
   RAILS_VERSION = '~> 5.1'.freeze
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip.freeze
-  VERSION = '0.2.0'.freeze
+  VERSION = '0.3.0'.freeze
 end
